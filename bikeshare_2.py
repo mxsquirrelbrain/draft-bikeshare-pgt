f35ffae6c4589573b303a275c281aad9cdfa0e85
mxsquirrelbrain <102433020+mxsquirrelbrain@users.noreply.github.com>
2022-03-26 13:33:20 -0700

    Update bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,13 +17,42 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    city=''
+    while True:
+        city = input('Please select one city to evaluate: Chicago, New York City, Washington: ').lower()
+        if city not in CITY_DATA:
+            print('Please enter your selection exactly.')
+        else:
+            print('Thank you for selecting: {}'.format(city))
+            break
 
     # get user input for month (all, january, february, ... , june)
+    months = ['january', 'february', 'march', 'april', 'may', 'june']
 
+    while True:
+        month = input('Please select one month between January through June or type all to see data for all months: ').lower()
+        if month == 'all':
+            print('Thank you for selecting all months.')
+            break
+        elif month not in months:
+            print('Please enter one of these months: January, February, March, April, May, June or type All.')
+        else:
+            print('Thank you for selecting: {}'.format(month))
+            break
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
+    days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
+    while True:
+        day = input('Please select a day of the week by name or type All to see data for all days of the week:').lower()
+        if day == 'all':
+            print('Thank you for selecting all days.')
+            break
+        elif day not in days:
+            print('Please enter a day of the week or type All.')
+        else:
+            print('Thank you for selecting: {}'.format(day))
+            break
 
     print('-'*40)
     return city, month, day
@@ -40,10 +69,31 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    # load data file into a dataframe
+    df = pd.read_csv(CITY_DATA[city])
 
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.weekday #_name
+
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1
+
+        # filter by month to create the new dataframe
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df['day_of_week'] == day.title()]
 
     return df
-
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
@@ -52,13 +102,19 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
-
+    print('Bikeshare users most often used the service in the month of {}.')
 
     # display the most common day of week
-
+    print('Bikeshare users most often used the service on {}.')
 
     # display the most common start hour
+        # extract hour from the Start Time column to create an hour column
+        df['hour'] = df['Start Time'].dt.hour
 
+        # find the most popular hour
+        popular_hour = df['hour'].mode()[0]
+
+        print('Bikeshare users most often started their journey in the {} hour.').format(popular_hour)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -124,9 +180,9 @@ def main():
         df = load_data(city, month, day)
 
         time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        #station_stats(df)
+        #trip_duration_stats(df)
+        #user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
